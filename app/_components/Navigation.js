0b20ecad7e06763a20d27c4cd0690773ae3a1648
Delight Sheriff <delightsheriff@gmail.com>
2024-06-19 23:05:25 +0100

    implemented user sessions

--- app/_components/Navigation.js
+++ app/_components/Navigation.js
@@ -1,6 +1,11 @@
 import Link from "next/link";
+import { auth } from "../_lib/auth";
 
-export default function Navigation() {
+export default async function Navigation() {
+  //getting the session from the auth function{login status, user data, etc.}
+  const session = await auth();
+  // console.log(session);
+
   return (
     <nav className="z-10 text-xl">
       <ul className="flex gap-16 items-center">
@@ -21,12 +26,27 @@ export default function Navigation() {
           </Link>
         </li>
         <li>
-          <Link
-            href="/account"
-            className="hover:text-accent-400 transition-colors"
-          >
-            Guest area
-          </Link>
+          {session?.user?.image ? (
+            <Link
+              href="/account"
+              className="hover:text-accent-400 transition-colors flex items-center gap-4"
+            >
+              <img
+                src={session.user.image}
+                alt="User avatar"
+                className="w-8 h-8 rounded-full"
+                referrerPolicy="no-referrer" //to correctly display images coming from google
+              />
+              <span> Guest area</span>
+            </Link>
+          ) : (
+            <Link
+              href="/account"
+              className="hover:text-accent-400 transition-colors"
+            >
+              Guest area
+            </Link>
+          )}
         </li>
       </ul>
     </nav>
