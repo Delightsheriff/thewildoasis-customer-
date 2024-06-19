d9d54d106592be5b9aeb888b59d1e8e206f5ab46
Delight Sheriff <delightsheriff@gmail.com>
2024-06-20 00:13:23 +0100

    implemented signin & signout button

--- app/_lib/actions.js
+++ app/_lib/actions.js
@@ -1,10 +1,15 @@
 "use server";
 
-import { signIn } from "./auth";
+import { signIn, signOut } from "./auth";
 
 ///server actions
 
 ///sign in action
 export async function signInAction() {
   await signIn("google", { redirectTo: "/account" });
+}
+
+///sign out action
+export async function signOutAction() {
+  await signOut({ redirectTo: "/" });
 }
